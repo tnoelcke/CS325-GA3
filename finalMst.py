7c14ffd4a16a8d9e04c54e8d25372cd112482331
Thomas <noelcket@oregonstate.edu>
2017-11-14 21:19:46 -0800

    Almost There

--- finalMst.py
+++ finalMst.py
@@ -1,5 +1,7 @@
 import heapq
 from heapq import heappush, heappop
+import copy
+from copy import deepcopy
 
 #pretty prints the forest for debuging.
 def displayForest(forest):
@@ -56,9 +58,10 @@ def findMst(heap, forest, edgesAdded, V):
 	while numEdge < V - 1:
 		safeEdge = heappop(heap)
 		print(safeEdge)
+		print(numEdge)
 		#if the two vertices aren't in the same component
 		if forest[safeEdge[1]][0] != forest[safeEdge[2]][0]:
-			numEdge = numEdge + 1
+			print("SafeEdge Found")
 			edgesAdded.append(safeEdge)
 			#label the connected components
 			reLabel(forest, safeEdge[1], safeEdge[2])
@@ -66,41 +69,73 @@ def findMst(heap, forest, edgesAdded, V):
 			addEdge(forest, safeEdge[1], safeEdge[2])
 			#add weight
 			weight = weight + safeEdge[0]
-	print(weight)
+			numEdge = numEdge + 1
+	return weight
 	
 #Lebels all the vertices in a component. Using DFS
 def dfsLabel(graph, start, label):
-	visted = set()
+	visited = []
 	stack = [start]
 	while stack:
 		vertex = stack.pop()
-		if vertex not in visted:
-			visted.add(vertex)
+		if vertex not in visited:
+			visited.append(vertex)
 			graph[vertex] = (label, graph[vertex][1])
 			stack.extend(graph[vertex][1])
-	
+			
 
-#Labels all the connected compontes in a graph
-def relabelComponents(forest):
-	allLebeled = False
-	dfsLabel(forest, 0, 0)
+			
 
 #given the mst in the forest along with a list of edges in the mst
 #and the remaining edges in the heap left over from finding the mst
 #this function finds the next mst.
-def findNextMst(forest, edgesAdded, heap):
-	for i in range(0, len(edgesAdded))
-		tempEdge = edgesAdded.pop()
-		
+def findNextMst(forest, edgesAdded, heap, weight):
+	minWeight = 0
+	edgeAdded = []
+	tempWeight = 0
+	heapState = []
+	graphState = []
+	for i in range(0, len(edgesAdded)):
+		tempHeap = deepcopy(heap)
+		tempEdge = edgesAdded[i]
+		tempWeight = weight - tempEdge[0]
+		removeEdge(forest, tempEdge)
+		#We know there will only ever be two components we need to label so pick an edge
+		#and preform the label the other edges should already all be labeled the same thing.
+		#this will result in two unique componets.
+		dfsLabel(forest, 0, i)
+		tempEdgeAdded = []
+		temp1 = findMst(tempHeap, forest, tempEdgeAdded, 2)
+		print(temp1)
+		temp = tempWeight + temp1
+		print(temp)
+		if minWeight == 0 or minWeight > temp:
+			minWeight = temp
+			edgeAdded = tempEdgeAdded
+			heapState = deepcopy(tempHeap)
+			graphState = deepcopy(forest)
+		#set up for next iteration
+		print(tempEdge)
+		print(tempEdgeAdded)
+		addEdge(forest, tempEdge[1], tempEdge[2])
+		removeEdge(forest, tempEdgeAdded[0])
+	print(minWeight)
 	
+	
+	
+
 graph = [line.rstrip('\n').split(',') for line in open('input.txt','r')]
 totalVetices = int(graph[0][0])
 del graph[0]
 
 forest = []
 edgesAdded = []
+weight = 0
 heap = setUpHeap(graph)
-findMst(heap, forest, edgesAdded,totalVetices)
-removeEdge(forest, edgesAdded[0])
-relabelComponents(forest)
+weight = findMst(heap, forest, edgesAdded,totalVetices)
+print("First: ", weight)
+displayForest(forest)
+findNextMst(forest, edgesAdded, heap, weight)
+
+
 
